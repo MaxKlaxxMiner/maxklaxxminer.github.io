811d666b28b79b453a31243c5b17d0c032062ba2
MaxKlaxx <max@tetrisk.de>
2022-07-29 19:02:00 +0200

    +full playable keyboard

--- main.js
+++ main.js
@@ -18,9 +18,59 @@ const wg = {
     workletWatError: "",
     workletWatWasm: null,
     workletWatReady: false,
+    workletWatVersion: 0,
 };
 window["wg"] = wg;
 const version = Date.now();
+const keyTones = {
+    Backquote: 46,
+    Tab: 47,
+    KeyQ: 48,
+    Digit2: 49,
+    KeyW: 50,
+    Digit3: 51,
+    KeyE: 52,
+    KeyR: 53,
+    Digit5: 54,
+    KeyT: 55,
+    Digit6: 56,
+    KeyY: 57,
+    Digit7: 58,
+    KeyU: 59,
+    KeyI: 60,
+    Digit9: 61,
+    KeyO: 62,
+    Digit0: 63,
+    KeyP: 64,
+    BracketLeft: 65,
+    Equal: 66,
+    BracketRight: 67,
+    Backspace: 68,
+    Enter: 69,
+    ShiftLeft: 33,
+    CapsLock: 34,
+    IntlBackslash: 35,
+    KeyZ: 36,
+    KeyS: 37,
+    KeyX: 38,
+    KeyD: 39,
+    KeyC: 40,
+    KeyV: 41,
+    KeyG: 42,
+    KeyB: 43,
+    KeyH: 44,
+    KeyN: 45,
+    KeyJ: 46,
+    KeyM: 47,
+    Comma: 48,
+    KeyL: 49,
+    Period: 50,
+    Semicolon: 51,
+    Slash: 52,
+    ShiftRight: 53,
+    Backslash: 54,
+};
+let baseOctave = 1;
 function initUserEvents() {
     onclick = () => {
         wg.userInput = true;
@@ -30,25 +80,53 @@ function initUserEvents() {
         wg.userInput = true;
         initAudio();
     };
-    onkeydown = (e) => {
-        switch (e.key) {
+    onkeydown = e => {
+        switch (e.key) { // special keys = no user input
             case "Alt":
             case "Control":
             case "Shift":
             case "CapsLock":
             case "Escape":
             case "ScrollLock":
-            case "NumLock":
-                return;
+            case "NumLock": {
+                break;
+            }
+            default: {
+                wg.userInput = true;
+                initAudio();
+                break;
+            }
         }
-        wg.userInput = true;
-        initAudio();
+        document.title = "key: " + e.code;
+        if (keyTones[e.code]) {
+            toneStart(keyTones[e.code] + baseOctave * 12, true);
+            e.preventDefault();
+        }
+        if (e.code === "PageUp") {
+            baseOctave++;
+            if (baseOctave > 4)
+                baseOctave = 4;
+            toneKill();
+            e.preventDefault();
+        }
+        if (e.code === "PageDown") {
+            baseOctave--;
+            if (baseOctave < -2)
+                baseOctave = -2;
+            toneKill();
+            e.preventDefault();
+        }
+    };
+    onkeyup = e => {
+        if (keyTones[e.code]) {
+            toneEnd(keyTones[e.code] + baseOctave * 12);
+            e.preventDefault();
+        }
     };
     // --- note-buttons ---
     const addNoteButtons = (noteButtons, hq) => {
         for (let i = 0; i < noteButtons.length; i++) {
             const button = noteButtons[i];
-            console.log(button);
             const code = parseInt(button.dataset.midicode);
             if (code) {
                 let active = false;
@@ -185,8 +263,13 @@ function workletReceiveMessage(msg) {
             workletSendMessage(null);
             break;
         }
-        case "ok: watWasmReady": {
-            wg.workletWatReady = true;
+        default: {
+            if (msg.data.indexOf("ok: watWasmReady ") === 0) {
+                wg.workletWatReady = true;
+                wg.workletWatVersion = msg.data.substr(17);
+                break;
+            }
+            console.log("unknown message: \"" + msg.data + "\"");
             break;
         }
     }
@@ -197,6 +280,9 @@ function toneStart(midiCode, hq) {
 function toneEnd(midiCode) {
     workletSendMessage({ t: "toneEnd", val: midiCode });
 }
+function toneKill() {
+    workletSendMessage({ t: "toneKill" });
+}
 function updateStat() {
     const msg = txt => {
         const el = document.getElementById("stat");
@@ -204,7 +290,7 @@ function updateStat() {
             el.innerText = txt;
     };
     if (!wg.userInput)
-        return msg("wait for user input...");
+        return msg("wait for first user input...");
     if (!wg.mainGoInit)
         return msg("initMainGo() not started");
     if (wg.mainGoError)
@@ -237,7 +323,10 @@ function updateStat() {
         return msg("load worklet.wasm...");
     if (!wg.workletWatReady)
         return msg("worklet.wasm not ready");
-    msg("run: " + wg.audioContext.currentTime.toFixed(1) + " s - " + wg.workletBlockCount16 + " blocks - " + (wg.workletBlockCount16 * 16 * 128 / 1000000).toFixed(2) + "M samples");
+    msg("run: " + wg.audioContext.currentTime.toFixed(1) + " s - " + (wg.workletBlockCount16 * 16) + " blocks - " + (wg.workletBlockCount16 * 16 * 128 / 1000000).toFixed(2) + "M samples (wat version: " + (wg.workletWatVersion / 10000).toFixed(4) + ")");
+    const el = document.getElementById("octave");
+    if (el)
+        el.innerText = baseOctave.toString();
 }
 window.addEventListener("load", () => {
     setInterval(() => {
--- worklet.js
+++ worklet.js
@@ -10,8 +10,53 @@ const wg = {
     workletWatSamplesRight: null,
     workletWatReady: false,
 };
-const watMinVersion = 10002;
+const watMinVersion = 10004;
+class Tone {
+    constructor(midiCode, volume, hq) {
+        this.midiCode = midiCode;
+        this.ofsL = 0;
+        this.ofsR = 0;
+        this.incrL = (4294967296 / (44100 / (440 * Math.pow(2, (midiCode - 69 + 0.01) / 12))) + 0.5) | 0;
+        this.incrR = (4294967296 / (44100 / (440 * Math.pow(2, (midiCode - 69 - 0.01) / 12))) + 0.5) | 0;
+        this.sampler = hq ? wg.workletWat.squareHQ : wg.workletWat.squareLQ;
+        this.volume = volume > 2 ? volume | 0 : (8388608 * volume) | 0;
+    }
+    fill(ptrBufL, ptrBufR, sampleCount) {
+        this.ofsL = this.sampler(ptrBufL, sampleCount, this.incrL, this.ofsL);
+        this.ofsR = this.sampler(ptrBufR, sampleCount, this.incrR, this.ofsR);
+        wg.workletWat.volumeUpdate(ptrBufL, sampleCount, this.volume);
+        wg.workletWat.volumeUpdate(ptrBufR, sampleCount, this.volume);
+    }
+}
 console.log("worklet: start processor");
+const sampleCount = 128;
+const ptrOutputFloatL = 0;
+const ptrOutputFloatR = 512;
+const ptrMixL = 1024;
+const ptrMixR = 1024 + 512;
+const ptrTmpL = 2048;
+const ptrTmpR = 2048 + 512;
+//const noiseVolume = (8388608 * 0.02) | 0; // 2% noise background
+const noiseVolume = (8388608 * 0.00) | 0; // 0% noise background
+let noiseOfs = 0;
+let currentTones = {};
+function watFillBuffer(output) {
+    const wat = wg.workletWat;
+    noiseOfs = wat.noise(ptrMixL, sampleCount, 123456789, noiseOfs);
+    noiseOfs = wat.noise(ptrMixR, sampleCount, 123456789, noiseOfs);
+    wat.volumeUpdate(ptrMixL, sampleCount, noiseVolume); // volume update - left side
+    wat.volumeUpdate(ptrMixR, sampleCount, noiseVolume); // volume update - right side
+    for (const key in currentTones) {
+        const tone = currentTones[key];
+        tone.fill(ptrTmpL, ptrTmpR, sampleCount);
+        wat.mix(ptrMixL, ptrMixL, ptrTmpL, sampleCount);
+        wat.mix(ptrMixR, ptrMixR, ptrTmpR, sampleCount);
+    }
+    wat.convertIntSamplesToFloat32(ptrOutputFloatL, ptrMixL, sampleCount); // convert int -> float32 - left side
+    wat.convertIntSamplesToFloat32(ptrOutputFloatR, ptrMixR, sampleCount); // convert int -> float32 - right side
+    output[0].set(wg.workletWatSamplesLeft); // copy left-samples to output-buffer
+    output[1].set(wg.workletWatSamplesRight); // copy right-samples to output-buffer
+}
 function recMessage(event) {
     console.log("data:", event.data);
     switch (event.data.t) {
@@ -21,8 +66,8 @@ function recMessage(event) {
             const instance = new WebAssembly.Instance(module, importObject);
             wg.workletWat = instance.exports;
             wg.workletWatMem = new Uint8Array(wg.workletWat.mem.buffer);
-            wg.workletWatSamplesLeft = new Float32Array(wg.workletWat.mem.buffer, 0, 128);
-            wg.workletWatSamplesRight = new Float32Array(wg.workletWat.mem.buffer, 512, 128);
+            wg.workletWatSamplesLeft = new Float32Array(wg.workletWat.mem.buffer, ptrOutputFloatL, sampleCount);
+            wg.workletWatSamplesRight = new Float32Array(wg.workletWat.mem.buffer, ptrOutputFloatR, sampleCount);
             const version = wg.workletWat.version();
             if (version < watMinVersion) {
                 console.log("invalid worklet.wasm version: " + version);
@@ -30,47 +75,24 @@ function recMessage(event) {
             }
             console.log("worklet: run wat version: " + version);
             wg.workletWatReady = true;
-            wg.workletPort.postMessage("ok: watWasmReady");
+            wg.fillBuffer = watFillBuffer;
+            wg.workletPort.postMessage("ok: watWasmReady " + version);
             break;
         }
         case "toneStart": {
             const code = event.data.val;
-            if (wg.workletWatReady) {
-                let ofsL = 0;
-                let ofsR = 0;
-                const incrL = (4294967296 / (44100 / (440 * Math.pow(2, (code - 69 + 0.01) / 12))) + 0.5) | 0;
-                const incrR = (4294967296 / (44100 / (440 * Math.pow(2, (code - 69 - 0.01) / 12))) + 0.5) | 0;
-                const wat = wg.workletWat;
-                const func = event.data.hq ? wat.squareHQ : wat.squareLQ;
-                wg.fillBuffer = output => {
-                    ofsL = func(1024, 128, incrL, ofsL);
-                    ofsR = func(1536, 128, incrR, ofsR);
-                    wat.convertIntSamplesToFloat32(0, 1024, 128); // convert int -> float32 - left side
-                    wat.convertIntSamplesToFloat32(512, 1536, 128); // convert int -> float32 - right side
-                    output[0].set(wg.workletWatSamplesLeft); // copy left-samples to output-buffer
-                    output[1].set(wg.workletWatSamplesRight); // copy right-samples to output-buffer
-                    output.forEach(channel => {
-                        for (let i = 0; i < channel.length; i++) {
-                            channel[i] *= 0.1; // volume = 10%
-                        }
-                    });
-                };
-            }
-            else {
-                wg.fillBuffer = output => {
-                    output.forEach(channel => {
-                        for (let i = 0; i < channel.length; i++) {
-                            channel[i] = Math.random() * 2 - 1;
-                            channel[i] *= 0.1;
-                        }
-                    });
-                };
+            if (!currentTones[code]) {
+                currentTones[code] = new Tone(code, 0.1, event.data.hq);
             }
             break;
         }
         case "toneEnd": {
-            //const code = event.data.val;
-            wg.fillBuffer = null;
+            const code = event.data.val;
+            delete currentTones[code];
+            break;
+        }
+        case "toneKill": {
+            currentTones = {};
             break;
         }
     }
